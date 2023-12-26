ae07262f5d2ddaa83b42201dad150ad538147ce4
vprorok <vprorok@ivi.ru>
2023-12-26 10:38:19 +0300

    Добавлен лейбл со счетчиком нажатий и кнопка для учета нажатий

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -2,18 +2,26 @@
 //  ViewController.swift
 //  Counter
 //
-//  Created by Владислав Пророк on 25.12.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterIncreaseButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    var counter: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter+=1
+        counterLabel.text = String("Значение счетчика: " + String(counter) )
+        
+    }
+    
 }
 
